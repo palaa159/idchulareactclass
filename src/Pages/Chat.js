437a7bac4e02c70a8ce9285cc427f2ef99f0af64
Apon Palanuwech <palaa159@newschool.edu>
2017-09-12 14:44:11 +0700

    - added alert for user id

--- src/Pages/Chat.js
+++ src/Pages/Chat.js
@@ -47,17 +47,25 @@ class Chat extends React.Component {
       <div className="columns">
         <div className="column">
           <div className="content">
+            <h2>Users:</h2>
             { this.state.users && Object.keys(this.state.users)
                 // .reverse()
                 .sort((key) => this.state.users[key].status)
                 .map((key, i) =>
-                  <li key={i}>
+                  <div key={i} onClick={() => alert(`
+                      ${this.state.users[key].emoji}
+                      id: ${key}
+                      last_seen: ${moment(this.state.users[key].lastSeen).fromNow()}
+                    `)}>
                     { this.props.userId === key &&
                       <span>☝️ </span>
                     }
                     <span className="is-size-4">{this.state.users[key].emoji} </span>
-                    <span className="tag is-rounded">{this.state.users[key].status === 'online' ? '💚 ' : '🔴 '} {this.state.users[key].status}</span>
-                  </li>
+                    <span className="tag is-rounded">
+                      {this.state.users[key].status === 'online' ? '💚 ' : ''}
+                      {this.state.users[key].status}
+                    </span>
+                  </div>
               )
             }
           </div>
@@ -80,7 +88,13 @@ class Chat extends React.Component {
           </div>
           <div className="chatFeed">
             { this.state.chatLog && Object.keys(this.state.chatLog).reverse().map((key, i) =>
-                <div className="chatLine" key={i}>
+                <div 
+                  className="chatLine" 
+                  key={i} 
+                  onClick={() => alert(`
+                    ${this.state.users[this.state.chatLog[key].sender.id].emoji}
+                    id: ${this.state.chatLog[key].sender.id}
+                  `)}>
                   <span className="is-size-4">{this.state.users[this.state.chatLog[key].sender.id].emoji}</span>
                   <span className="is-size-4"> {this.state.chatLog[key].message}</span>
                   <span>&nbsp;&nbsp;&nbsp;</span>
