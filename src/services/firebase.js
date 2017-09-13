c975ffe2c147d44706c652d6afed05bb6fdac533
Apon Palanuwech <palaa159@newschool.edu>
2017-09-13 20:41:46 +0700

    - remove firebase config

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,13 +1,5 @@
 import firebase from 'firebase'
-
-const config = {
-  apiKey: "AIzaSyBJc_bqk13PnAyx3wPTk3cyw-iOxc__ayI",
-  authDomain: "namoproject-e5062.firebaseapp.com",
-  databaseURL: "https://namoproject-e5062.firebaseio.com",
-  projectId: "namoproject-e5062",
-  storageBucket: "namoproject-e5062.appspot.com",
-  messagingSenderId: "515394613668"
-}
+import config from './config'
 
 const auth = firebase.auth
 const provider = new firebase.auth.FacebookAuthProvider()
