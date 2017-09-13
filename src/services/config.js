e0a35ec6ab51eb8a095695f79ab08d1831e8afc6
Apon Palanuwech <palaa159@newschool.edu>
2017-09-13 23:48:53 +0700

    - WIP

---
+++ src/services/config.js
@@ -0,0 +1,10 @@
+const config = {
+  apiKey: "AIzaSyBJc_bqk13PnAyx3wPTk3cyw-iOxc__ayI",
+  authDomain: "namoproject-e5062.firebaseapp.com",
+  databaseURL: "https://namoproject-e5062.firebaseio.com",
+  projectId: "namoproject-e5062",
+  storageBucket: "namoproject-e5062.appspot.com",
+  messagingSenderId: "515394613668"
+}
+
+export default config
